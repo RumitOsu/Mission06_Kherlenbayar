daf3444e67977296c38537a298517dfdd9b143a4
RumitOsu <timurkherlenbayar@gmail.com>
2024-02-16 23:24:51 -0700

    Add project files.

---
+++ Mission06_Kherlenbayar/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission06_Kherlenbayar.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ Mission06_Kherlenbayar/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission06_Kherlenbayar.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        
+        [Required]
+        public string Title { get; set; }
+        
+        [Required]
+        public string Director { get; set; }
+        
+        [Required]
+        public int Year { get; set; }
+        
+        [Required]
+        public string Rating { get; set; }
+        
+        public bool Edited { get; set; }
+
+        // Make these properties nullable
+        public string? LentTo { get; set; }
+
+        [MaxLength(25)]
+        public string? Notes { get; set; }
+    }
+}
