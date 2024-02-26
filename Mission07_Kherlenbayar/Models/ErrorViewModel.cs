7de58c02867a6c925e58c18957e48fc53eacd80d
RumitOsu <103869313+RumitOsu@users.noreply.github.com>
2024-02-26 17:29:59 -0600

    Delete Mission07_Kherlenbayar directory

--- Mission07_Kherlenbayar/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mission07_Kherlenbayar.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
