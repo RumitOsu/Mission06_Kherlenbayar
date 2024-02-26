cfe8071c0dbd6d84537053ff3888bc473aabb0c4
RumitOsu <timurkherlenbayar@gmail.com>
2024-02-26 16:55:04 -0700

    Revert "Delete Mission07_Kherlenbayar directory"
    This reverts commit 7de58c02867a6c925e58c18957e48fc53eacd80d.

---
+++ Mission07_Kherlenbayar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission07_Kherlenbayar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
